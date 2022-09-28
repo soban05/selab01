5696303b5b76ca0a50e10028c34fcfccaad7fae1
soban05 <114394055+soban05@users.noreply.github.com>
2022-09-28 23:34:09 +0500

    Create cal03.py

---
+++ cal03.py
@@ -0,0 +1,13 @@
+def add(num1, num2):
+  return num1+num2
+
+def sub(num1, num2):
+  return num1-num2
+
+def mul(num1, num2):
+  return num1*num2
+
+res = add (5,3)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
